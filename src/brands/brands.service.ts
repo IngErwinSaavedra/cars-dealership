74c37df66213ac26fa7d6c65e5a89bb41253038f
Erwin Saavedra <ing.erwin.saavedra@gmail.com>
2023-05-13 14:56:51 -0400

    Course Project Car Dealership Successfully

---
+++ src/brands/brands.service.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@nestjs/common';
+import { CreateBrandDto } from './dto/create-brand.dto';
+import { UpdateBrandDto } from './dto/update-brand.dto';
+import { Brand } from './entities/brand.entity';
+
+@Injectable()
+export class BrandsService {
+  private brands: Brand[] = [
+    // {
+    //     id: uuid(),
+    //     brand: 'Toyota',
+    //     model: 'Corolla'
+    // },
+];
+  create(createBrandDto: CreateBrandDto) {
+    return 'This action adds a new brand';
+  }
+
+  findAll() {
+    return `This action returns all brands`;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} brand`;
+  }
+
+  update(id: number, updateBrandDto: UpdateBrandDto) {
+    return `This action updates a #${id} brand`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} brand`;
+  }
+
+  fillBrandsWithSeedData(brands: Brand[]) {
+    this.brands = brands;
+  }
+}
---
+++ src/seed/seed.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@nestjs/common';
+import { CarsService } from 'src/cars/cars.service';
+import { BrandsService } from 'src/brands/brands.service';
+
+import { CARS_SEED } from './data/cars.seed';
+import { BRANDS_SEED } from './data/brands.seed';
+
+@Injectable()
+export class SeedService {
+
+  constructor(
+    private readonly carsService: CarsService,
+    private readonly brandService: BrandsService
+  ) {}
+
+
+  populateDB() {
+
+    // CARS_SEED
+    // BRANDS_SEED
+    this.carsService.fillCarsWithSeedData( CARS_SEED );
+    this.brandService.fillBrandsWithSeedData( BRANDS_SEED );
+    return 'SEED Executed Successfully';
+  }
+
+}
